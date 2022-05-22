bfc27ebb6ce88206a6733a12c63a1a794792bd26
Tanvirmehedi <tjmehedi201@gmail.com>
2022-05-23 04:05:18 +0600

    404 page implement

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
-  return <div>not found</div>;
+  return (
+    <div>
+      <div class="card lg:card-side bg-base-100 shadow-xl h-[90vh]">
+        <figure>
+          <img
+            src="https://cdn.dribbble.com/users/932640/screenshots/2470471/jq.gif"
+            alt="Album"
+          />
+        </figure>
+        <div class="card-body">
+          <div class="card-actions justify-center items-center h-full">
+            <Link to="/" class="btn btn-primary">
+              Back To Home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PageNotFound;
