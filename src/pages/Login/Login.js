062f5734ba6f4a3db43a5f94fc8eaab9735827a2
Tanvirmehedi <tjmehedi201@gmail.com>
2022-05-23 01:51:30 +0600

    React Hook Form Use And Create Login Functionality

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,45 +1,114 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {
+  useSignInWithEmailAndPassword,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
+import { useForm } from "react-hook-form";
+import auth from "../../firebase.init";
 
 const Login = () => {
+  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  if (gUser) {
+    console.log(gUser);
+  }
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <div>
-      <div class="hero min-h-screen bg-base-200">
-        <div class="hero-content flex-col lg:flex-row-reverse">
-          <div class="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
-            <div class="card-body">
-              <h2 className="text-xl text-center font-bold text-accent uppercase">
-                Login
-              </h2>
-              <div class="form-control">
-                <label class="label">
-                  <span class="label-text">Email</span>
+    <div className="my-10">
+      <div className="card max-w-xs bg-base-100 shadow-xl container mx-auto">
+        <div className="card-body">
+          <div className="mx-auto">
+            <h2 className="text-center text-2xl font-bold">Login</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="form-control w-full max-w-xs">
+                <label className="label">
+                  <span className="label-text">What is your Email?</span>
                 </label>
                 <input
-                  type="text"
-                  placeholder="email"
-                  class="input input-bordered"
+                  type="email"
+                  placeholder="Type Your Email"
+                  className="input input-bordered w-full"
+                  {...register("email", {
+                    required: {
+                      value: true,
+                      message: "Email is required",
+                    },
+                    pattern: {
+                      value: /(.+)@(.+){2,}\.(.+){2,}/,
+                      message: "Provide a valid email ",
+                    },
+                  })}
                 />
+                <label className="label">
+                  {errors.email?.type === "required" && (
+                    <span className="label-text-alt text-red-600">
+                      {errors.email.message}
+                    </span>
+                  )}
+                  {errors.email?.type === "pattern" && (
+                    <span className="label-text-alt text-red-600">
+                      {errors.email.message}
+                    </span>
+                  )}
+                </label>
               </div>
-              <div class="form-control">
-                <label class="label">
-                  <span class="label-text">Password</span>
+              {/* Email Field End  */}
+
+              <div className="form-control w-full max-w-xs">
+                <label className="label">
+                  <span className="label-text">What is your Password?</span>
                 </label>
                 <input
-                  type="text"
-                  placeholder="password"
-                  class="input input-bordered"
+                  type="password"
+                  placeholder="Type Your Password"
+                  className="input input-bordered w-full max-w-xs"
+                  {...register("password", {
+                    required: {
+                      value: true,
+                      message: "Password is required",
+                    },
+                    minLength: {
+                      value: 6,
+                      message: "Give 6 digits or longer",
+                    },
+                  })}
                 />
-                <label class="label">
-                  <Link to="#" class="label-text-alt link link-hover">
-                    Forgot password?
-                  </Link>
+                <label className="label">
+                  {errors.password?.type === "required" && (
+                    <span className="label-text-alt text-red-600">
+                      {errors.password.message}
+                    </span>
+                  )}
+                  {errors.password?.type === "minLength" && (
+                    <span className="label-text-alt text-red-600">
+                      {errors.password.message}
+                    </span>
+                  )}
                 </label>
               </div>
-              <div class="form-control mt-6">
-                <button class="btn btn-primary">Login</button>
-              </div>
-            </div>
+
+              <input
+                className="btn btn-primary w-full max-w-xs"
+                type="submit"
+                value="Login"
+              />
+            </form>
+            <div className="divider">OR</div>
+            <button
+              onClick={() => signInWithGoogle()}
+              className="btn btn-outline w-full max-w-xs"
+            >
+              Continue With Google
+            </button>
           </div>
         </div>
       </div>
